a6e1d6aea8696cf1982639cd68a275c8887c6ced
RafaelFelps <rafaelfelps62@gmail.com>
2021-03-11 18:09:16 -0300

    Integração com a API do github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,24 +1,32 @@
 import { RepositoryItem } from "./RepositoryItem";
-import '../styles/repositories.scss'
+import "../styles/repositories.scss";
+import { useState, useEffect } from "react";
+
+// https://api.github.com/users/RafaelFelps/repos
 
 const repository = {
-    name: 'Unform',
-    description: 'Forms in reactooooooooou',
-    link: 'https://github.com/unform/unform'
-}
-
+  name: "Unform",
+  description: "Forms in reactooooooooou",
+  link: "https://github.com/unform/unform",
+};
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/RafaelFelps/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-
-        <RepositoryItem repository= {repository} />
-
-        <RepositoryItem repository= {repository} />
-        <RepositoryItem repository= {repository}/>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   );
